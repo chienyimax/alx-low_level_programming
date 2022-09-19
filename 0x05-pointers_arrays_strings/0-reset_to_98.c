ff856745e6150bd1df2fb7c323c812fa524332b8
chienyimax <chienyiokeziehumphery@gmail.com>
2022-09-19 06:24:16 -0700

    Task 1 Done

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * create a function that has a void reset_to_98(int *n)
+ * the value will be pointing to 98
+ *  pointer to an int
+ * Return 0;
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
